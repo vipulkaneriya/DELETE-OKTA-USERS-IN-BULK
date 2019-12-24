b1c28c75ff13db57b0215415ae7ec2457b9727b6
Kaneriya <VKaneriyaOF1693@leggmason.com>
2019-12-24 14:32:17 -0500

    Added minor changes

--- delete_okta_users.py
+++ delete_okta_users.py
@@ -7,22 +7,21 @@ import threading
 
 
 """
-    This code is to DELETE the Okta users in BULK based on its status. It will
-    write user detail output in csv file.
+    This code is to DELETE the Okta users in BULK based on its status. It will write user detail output in csv file.
 
     This code uses multi-threading to furiously delete the Okta users.
 
-	Please update the environment variables in a file named '.env' as below:
-		OKTA_URL        [https://<>.okta.com]
-		OKTA_API_TOKEN  [Use your API Token which has privilege to delete users ]
+    Please update the environment variables in a file named '.env' as below:
+        OKTA_URL        [https://<>.okta.com]
+        OKTA_API_TOKEN  [Use your API Token which has privilege to delete users ]
 
     Please initialize the variables as per your requirement:
-		DELETE_FLAG   [YES | NO]
-		GETUSER_FLAG  [YES | NO]
+        DELETE_FLAG   [YES | NO]
+        GETUSER_FLAG  [YES | NO]
         DELETE_FILTER   ["DEPROVISIONED" (Deactivated) | "PROVISIONED" | "SUSPENDED" | "STAGED" | "ACTIVE"]
             (Delete users based on its current status.)
-		GETUSER_FILTER  ["DEPROVISIONED" (Deactivated) | "PROVISIONED" | "SUSPENDED" | "STAGED" | "ACTIVE"]
-		    (to list all Staged users before deleting or activating users)
+        GETUSER_FILTER  ["DEPROVISIONED" (Deactivated) | "PROVISIONED" | "SUSPENDED" | "STAGED" | "ACTIVE"]
+            (to list all Staged users before deleting or activating users)
 
     Developed By : Vipul Kaneriya.
 """
